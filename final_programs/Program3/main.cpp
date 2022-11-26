cef0df7bb1e617c175532e40bb08d2501365dabe
Mohammad Kashif <iamkashif20@gmail.com>
2022-11-26 09:34:40 +0530

    modified the printing format

--- final_programs/Program3/main.cpp
+++ final_programs/Program3/main.cpp
@@ -1,25 +1,16 @@
 #include <iostream>
 using namespace std;
-/*
-
- Program 3: Write a C++ program to perform the addition of two time objects in hour and minute
-format, display the result in hour: minute format using object as a function argument.
-
- */
 
 class Time{
 private:
     int hr , min;
 public:
-    Time(){
-        hr=0;
-        min=0;
-    }
     Time(int hour,int minute){
         hr =hour;
         min = minute;
     }
     void operator + (Time obj){
+        cout << "Performing addition of two time objects" <<endl;
         min = min + obj.min;
         int r = min/60;
         if(r)
@@ -33,7 +24,7 @@ public:
 };
 
 int main() {
-    Time t1(04,30) ,t2(2,230) ,t3;
+    Time t1(04,30) ,t2(2,230);
     t1+t2;
     t1.showData();
     return 0;
